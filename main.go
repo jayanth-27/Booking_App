63a1f9b999bd793b15ca440de8b0c1c7facacd9d
jayanth-27 <jayanth.reddy178@gmail.com>
2024-12-12 12:08:37 -0500

    User input and pointers

--- main.go
+++ main.go
@@ -7,5 +7,18 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 	fmt.Printf("Welcome to %v\n ", conferenceName)
-	fmt.Printf("We have total capacity of %v out of which, %v are remaining", conferenceTickets, remainingTickets)
+	var userName string
+	var userTickets int
+	var userEmail string
+	fmt.Println("Please enter your Name")
+	fmt.Scan(&userName)
+
+	fmt.Println("Please enter the number of tickets required")
+	fmt.Scan(&userTickets)
+
+	fmt.Println("Please enter your email")
+	fmt.Scan(&userEmail)
+
+	fmt.Printf("Thank you %v for booking %v tickets, you will receive the confirmation email to %v\n", userName, userTickets, userEmail)
+	fmt.Printf("We have total capacity of %v out of which, %v are remaining", conferenceTickets, remainingTickets-userTickets)
 }
